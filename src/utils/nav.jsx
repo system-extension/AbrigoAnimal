b281e5626f3c7e0f81f9c8cef7fb99b1aca1be10
Ítalo David <italo.silva@univille.br>
2026-09-24 19:31:59 -0300

    Primeiro commit

---
+++ src/utils/nav.jsx
@@ -0,0 +1,22 @@
+function Nav() {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
+}
